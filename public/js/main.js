a5c439e7465edf375fe6d237e9e67e336f112ad5
Jamila <jaja_mimi@hotmail.com>
2021-02-03 11:53:40 +0100

    valeur sur input fait

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,62 @@
 // recuperer tout les chiffres
 let chiffres = document.querySelectorAll('.chiffre');
 let operateur = document.querySelectorAll('.operation');
-
+let btnEgale = document.querySelector('.egale')
 let btnClear = document.querySelector('.clear')
 //recuperer champ d'affichage 
 let champAffich = document.querySelector('.number');
+let nb1
+let nb2
+let op
 
-
+// console.log(chiffres);
 chiffres.forEach(el =>{
     el.addEventListener("click", ()=> {
         champAffich.innerHTML += el.innerText
+        if (champAffich.innerText.length > 2) {
+            nb2 = champAffich.innerHTML.substr(0,1)
+        } else {
+            nb2 = champAffich.innerHTML            
+        }
+        // nb2 = champAffich.innerHTML
     })
 })
 
 operateur.forEach(el =>{
-    el.addEventListener('click', ()=> {
+    el.addEventListener('click', () => {
+        nb1 = nb2
+        champAffich.innerHTML=""
         champAffich.innerHTML += el.innerText
+        op= el.innerText
+
     })
 })
 
 btnClear.addEventListener('click', ()=> {
     champAffich.innerHTML=""
+})
+
+
+
+btnEgale.addEventListener('click', ()=> {
+    console.log(nb1);
+    console.log(op);
+    console.log(nb2);
+    switch (op) {
+        case (op ="+"):
+            console.log(`${nb1+nb2}`);
+            champAffich.innerHTML= nb1+nb2
+            break;
+        case (op ="-"):
+            console.log(`${nb1-nb2}`);
+            // champAffich.innerHTML= nb1+nb2
+            break;
+        case (op ="*"):
+            console.log(`${nb1*nb2}`);
+            // champAffich.innerHTML= nb1+nb2
+            break;
+    
+        default:
+            break;
+    }
 })
